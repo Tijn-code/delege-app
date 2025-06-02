919f293f97b70acd155328d37cb4dd9938ffd5ee
Tijn-code <tijn@delege.net>
2025-06-02 20:39:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,76 +1,46 @@
 
-from flask import Flask, render_template, request, redirect, url_for, session
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.secret_key = "delege_secret"
 
-sports = ["hardlopen", "kracht_boven", "kracht_onder", "padel"]
+# Hersteltijden in uren
+hersteltijden = {
+    "hardlopen":    {"hardlopen": 48, "kracht_boven": 12, "kracht_onder": 48, "padel": 36},
+    "kracht_boven": {"hardlopen": 6, "kracht_boven": 48, "kracht_onder": 6, "padel": 12},
+    "kracht_onder": {"hardlopen": 48, "kracht_boven": 6, "kracht_onder": 72, "padel": 24},
+    "padel":        {"hardlopen": 12, "kracht_boven": 6, "kracht_onder": 24, "padel": 24}
+}
 
-def is_allowed(chosen, last_done):
-    if chosen == "hardlopen":
-        if last_done.get("hardlopen", 3) <= 2:
-            return False
-        if last_done.get("kracht_onder", 3) <= 2:
-            return False
-        if last_done.get("padel", 3) <= 2:
-            return False
-        return True
-    elif chosen == "kracht_boven":
-        if last_done.get("kracht_boven", 3) <= 1:
-            return False
-        return True
-    elif chosen == "kracht_onder":
-        if last_done.get("kracht_onder", 3) <= 1:
-            return False
-        if last_done.get("hardlopen", 3) <= 1:
-            return False
-        if last_done.get("padel", 3) <= 1:
-            return False
-        return True
-    elif chosen == "padel":
-        return True
-    return False
-
-def suggest_alternatives(chosen, last_done):
-    allowed = []
-    for sport in sports:
-        if sport != chosen and sport != "padel" and is_allowed(sport, last_done):
-            allowed.append((sport, last_done.get(sport, 3)))
-    if not allowed:
-        return []
-    allowed.sort(key=lambda x: -x[1])
-    best_score = allowed[0][1]
-    best_options = [sport for sport, score in allowed if score == best_score]
-    return best_options
+# Categorie naar uren
+categorie_naar_uren = {
+    "0": 6,   # Vandaag
+    "1": 24,  # Gisteren
+    "2": 48   # Eergisteren of langer
+}
 
 @app.route("/")
 def index():
-    return render_template("index_buttons.html", sports=sports)
+    return render_template("index.html")
 
-@app.route("/start/<sport>")
-def start(sport):
-    session["chosen"] = sport
-    return redirect(url_for("vragen"))
+@app.route("/advies", methods=["POST"])
+def advies():
+    gekozen = request.form["keuze"]
+    advies_mogelijk = True
 
-@app.route("/vragen", methods=["GET", "POST"])
-def vragen():
-    if request.method == "POST":
-        antwoorden = {}
-        for sport in sports:
-            antwoorden[sport] = int(request.form.get(sport, 3))
-        session["answers"] = antwoorden
-        return redirect(url_for("resultaat"))
+    for sport in ["hardlopen", "kracht_boven", "kracht_onder", "padel"]:
+        tijd_cat = request.form.get(sport, "2")
+        uren_geleden = categorie_naar_uren.get(tijd_cat, 48)
+        vereiste_rust = hersteltijden[gekozen][sport]
 
-    return render_template("vragen_pagina.html", sports=sports)
+        if uren_geleden < vereiste_rust:
+            advies_mogelijk = False
+            break
 
-@app.route("/resultaat")
-def resultaat():
-    gekozen = session.get("chosen", "onbekend")
-    antwoorden = session.get("answers", {})
-    toegestaan = is_allowed(gekozen, antwoorden)
-    suggesties = suggest_alternatives(gekozen, antwoorden)
-    return render_template("resultaat.html", gekozen=gekozen, toegestaan=toegestaan, suggesties=suggesties)
+    if gekozen == "padel":
+        advies = "Paddelen kan, maar plan dit altijd van tevoren."
+    elif advies_mogelijk:
+        advies = f"Je kunt vandaag {gekozen.replace('_', ' ')} doen."
+    else:
+        advies = f"Beter geen {gekozen.replace('_', ' ')} vandaag."
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    return render_template("advies.html", advies=advies)
