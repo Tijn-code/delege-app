d2eb444ba1b02b4f6d6449c62fe18a0168b10153
Tijn-code <tijn@delege.net>
2025-06-02 21:09:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,7 @@ def resultaat():
     return render_template("resultaat.html", advies=advies)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
