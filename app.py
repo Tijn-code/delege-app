75258726a6be298223fa3a50fbbca9bdcfde8fa4
Tijn-code <tijn@delege.net>
2025-06-02 21:00:06 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Hersteltijden in uren
 hersteltijden = {
     "hardlopen":    {"hardlopen": 48, "kracht_boven": 12, "kracht_onder": 48, "padel": 36},
     "kracht_boven": {"hardlopen": 6, "kracht_boven": 48, "kracht_onder": 6, "padel": 12},
@@ -11,36 +10,39 @@ hersteltijden = {
     "padel":        {"hardlopen": 12, "kracht_boven": 6, "kracht_onder": 24, "padel": 24}
 }
 
-# Categorie naar uren
 categorie_naar_uren = {
-    "0": 6,   # Vandaag
-    "1": 24,  # Gisteren
-    "2": 48   # Eergisteren of langer
+    "0": 6,
+    "1": 24,
+    "2": 48
 }
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index_buttons.html")
 
-@app.route("/advies", methods=["POST"])
-def advies():
-    gekozen = request.form["keuze"]
+@app.route("/vragen", methods=["POST"])
+def vragen():
+    keuze = request.form.get("keuze")
+    return render_template("vragen_pagina.html", keuze=keuze)
+
+@app.route("/resultaat", methods=["POST"])
+def resultaat():
+    keuze = request.form.get("keuze")
     advies_mogelijk = True
 
     for sport in ["hardlopen", "kracht_boven", "kracht_onder", "padel"]:
         tijd_cat = request.form.get(sport, "2")
         uren_geleden = categorie_naar_uren.get(tijd_cat, 48)
-        vereiste_rust = hersteltijden[gekozen][sport]
-
-        if uren_geleden < vereiste_rust:
+        vereist = hersteltijden[keuze][sport]
+        if uren_geleden < vereist:
             advies_mogelijk = False
             break
 
-    if gekozen == "padel":
+    if keuze == "padel":
         advies = "Paddelen kan, maar plan dit altijd van tevoren."
     elif advies_mogelijk:
-        advies = f"Je kunt vandaag {gekozen.replace('_', ' ')} doen."
+        advies = f"Je kunt vandaag {keuze.replace('_', ' ')} doen."
     else:
-        advies = f"Beter geen {gekozen.replace('_', ' ')} vandaag."
+        advies = f"Beter geen {keuze.replace('_', ' ')} vandaag."
 
-    return render_template("advies.html", advies=advies)
+    return render_template("resultaat.html", advies=advies)
